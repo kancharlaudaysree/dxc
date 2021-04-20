959bc780791c42a6b0fbdc08f64d27b83bdd38be
kancharlaudaysree <47479833+kancharlaudaysree@users.noreply.github.com>
2021-04-20 08:04:45 +0530

    Add files via upload

---
+++ Exercises/SomethingIsWrong.java
@@ -0,0 +1,17 @@
+package dxc;
+public class SomethingIsWrong {
+	private static SomethingIsWrong myRect;
+
+	public static void main(String[] args) {
+	 
+myRect = null;
+System.out.println("myRect's area is " + myRect.area());
+
+    }
+
+private String area() {
+	return null;
+}
+
+}
+
