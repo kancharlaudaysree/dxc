f2121a52c52f2e31d356c6678d51b297a805c780
kancharlaudaysree <47479833+kancharlaudaysree@users.noreply.github.com>
2021-04-26 20:17:31 +0530

    Create findword.js

---
+++ JavascriptAsses/findword.js
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+function search_word(text, word){
+    
+    var x = 0, y=0;
+   
+    for (i=0;i< text.length;i++)
+        {
+        if(text[i] == word[0])
+            {
+            for(j=i;j< i+word.length;j++)
+               {
+                if(text[j]==word[j-i])
+                  {
+                    y++;
+                  }
+                if (y==word.length){
+                    x++;
+                }
+            }
+            y=0;
+        }
+    }
+   return "'"+word+"' was found "+x+" times.";
+}
+
+</script>
+
+</head>
+<body>
+<script>
+document.write(search_word('The quick brown fox', 'fox'));
+document. write("\n"); 
+document.write (search_word('aa, bb, cc, dd, aa', 'aa'));
+</script>
+
+</body>
+</html>
