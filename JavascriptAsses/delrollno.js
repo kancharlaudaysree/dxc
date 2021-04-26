3e94dd3d4a21f8c45683f79c5aab5ffa96b8996d
kancharlaudaysree <47479833+kancharlaudaysree@users.noreply.github.com>
2021-04-26 21:14:43 +0530

    Create delrollno.js

---
+++ JavascriptAsses/delrollno.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+<script>
+var student =  { 
+  name : "David Rayy", 
+  sclass : "VI", 
+  rollno : 12  };
+console.log(student);
+delete student.rollno;
+console.log(student);
+</script>
+</body>
+</html>
