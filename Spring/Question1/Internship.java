e205fed6692c16caf8d4ac946f5bbdd6a18d25ea
kancharlaudaysree <47479833+kancharlaudaysree@users.noreply.github.com>
2021-05-03 18:21:39 +0530

    Create Internship.java

---
+++ Spring/Question1/Internship.java
@@ -0,0 +1,6 @@
+package com;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class Internship {
+	ApplicationContext con= new ClassPathXmlApplicationContext("applicationContext.xml");
+}
