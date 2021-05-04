0dd8d911d4bcdd5e90043bb3104d0aa442b6c747
kancharlaudaysree <47479833+kancharlaudaysree@users.noreply.github.com>
2021-05-04 23:12:39 +0530

    Create SpringBoatwebappdemoApplication.java

---
+++ SpringBoat/SpringBoatwebappdemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.SpringBootwebappdemo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoatwebappdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoatwebappdemoApplication.class, args);
+	}
+
+}
